719bcbaf3191a23f30baef607402e9e619b72a57
tanishqa <tanishqasharma09@gmail.com>
2026-01-22 21:43:59 +0530

    [tanishqa sharma].add custom module

---
+++ assignment2/exercise2/main.js
@@ -0,0 +1,5 @@
+const stringUtils = require("./q2");
+
+console.log(stringUtils.capitalize("hello world"));
+console.log(stringUtils.reverseString("nodejs"));
+console.log("Vowels:", stringUtils.countVowels("JavaScript"));
